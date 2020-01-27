ea6eb5dd410b323f77b449042b303275b34179d7
hyun-wook ha <ha0146@naver.com>
2020-01-27 21:12:20 +0900

    Add test for perf

--- server/crawler.rs
+++ server/crawler.rs
@@ -210,4 +210,48 @@ mod tests {
         let ans2 = Tools::comb_sub(&subjects, &fix_subs, &mut req_subs, &mut sel_subs);
         println!("{:?}", ans2.unwrap().unwrap().len());
     }
+
+    #[test]
+    fn old_combination() {
+        let a = SubjectResponse::from_file("foo.txt".to_string());
+
+        let subject_vec = a.to_subject_vector();
+        let combinator = Tools::SubjectCombinator::new(&subject_vec);
+        // let fix_subs = vec![("SE324a".to_string(), 0), ("SE334a".to_string(), 0), ("SE380".to_string(), 0), ("HL303".to_string(), 31)];
+        // let mut req_subs = vec!["HL203".to_string(), "HL204".to_string(), "HL305".to_string()];
+        // let mut sel_subs = vec!["HL320".to_string()];
+    
+        let fix_subs = vec![];
+        let mut req_subs = vec!["SE102a".to_string(), "SE105a".to_string(), "SE106".to_string(), "SE108a".to_string(), "SE109".to_string(), "SE111".to_string(), "SE112".to_string(), "SE118".to_string()];
+        let mut sel_subs = vec![];
+    
+        let mut subjects = HashMap::new();
+        for sub in subject_vec.clone().into_iter() {
+            subjects.entry(sub.code.clone()).or_insert_with(Vec::new).push(sub);
+        }
+    
+        Tools::comb_sub(&subjects, &fix_subs, &mut req_subs, &mut sel_subs);
+    }
+
+    #[test]
+    fn new_combination() {
+        let a = SubjectResponse::from_file("foo.txt".to_string());
+
+        let subject_vec = a.to_subject_vector();
+        let combinator = Tools::SubjectCombinator::new(&subject_vec);
+        // let fix_subs = vec![("SE324a".to_string(), 0), ("SE334a".to_string(), 0), ("SE380".to_string(), 0), ("HL303".to_string(), 31)];
+        // let mut req_subs = vec!["HL203".to_string(), "HL204".to_string(), "HL305".to_string()];
+        // let mut sel_subs = vec!["HL320".to_string()];
+    
+        let fix_subs = vec![];
+        let mut req_subs = vec!["SE102a".to_string(), "SE105a".to_string(), "SE106".to_string(), "SE108a".to_string(), "SE109".to_string(), "SE111".to_string(), "SE112".to_string(), "SE118".to_string()];
+        let mut sel_subs = vec![];
+    
+        let mut subjects = HashMap::new();
+        for sub in subject_vec.clone().into_iter() {
+            subjects.entry(sub.code.clone()).or_insert_with(Vec::new).push(sub);
+        }
+    
+        combinator.comb_sub(&fix_subs, &mut req_subs, &mut sel_subs);
+    }
 }
