b7d78bf1bfe6fec7b9a947a4ed889a022ec82e1f
hyun-wook ha <ha0146@naver.com>
2020-01-31 20:34:29 +0900

    update to 2020 spring

--- server/main.rs
+++ server/main.rs
@@ -110,7 +110,7 @@ async fn main() -> std::io::Result<()> {
         subject_vec = Subject::load("data.json");
     }
     else {
-        let a = crawler::SubjectQuery::new(2019).fall().undergraduate().send().await.unwrap();
+        let a = crawler::SubjectQuery::new(2020).spring().undergraduate().send().await.unwrap();
         subject_vec = a.to_subject_vector();
         backend::Subject::Subject::save(&subject_vec, "data.json");
     }
@@ -152,7 +152,7 @@ async fn main() -> std::io::Result<()> {
         subject_vec = Subject::load("data.json");
     }
     else {
-        let a = crawler::SubjectQuery::new(2019).fall().undergraduate().send().await.unwrap();
+        let a = crawler::SubjectQuery::new(2020).spring().undergraduate().send().await.unwrap();
         subject_vec = a.to_subject_vector();
         backend::Subject::Subject::save(&subject_vec, "data.json");
     }
